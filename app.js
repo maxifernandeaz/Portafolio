92642a9cc8c0b3610b5e33f1b9e919c6faba1e35
maxifernandeaz <maximilianofernandezbaez@gmail.com>
2024-09-28 19:16:19 -0300

    subiendo porfatolio

---
+++ app.js
@@ -0,0 +1,29 @@
+// function para las url de los botones
+
+function openRepositorioEncriptador() {
+    window.open('https://github.com/maxifernandeaz/encriptadordetexto.git', '_blank');
+}
+
+function openDemoEncriptador() {
+    window.open('https://encriptadordetexto-alura-g7.netlify.app/', '_blank');
+}
+
+function openRepositorioAdministradorDeCitas() {
+    window.open('https://github.com/maxifernandeaz/administrador-citas.git', '_blank');
+}
+
+function openDemoAdministradorDeCitas() {
+    window.open('https://adminstradordecitas.netlify.app/', '_blank');
+}
+
+
+function openRepositorioTrancaStyle() {
+    window.open('https://github.com/maxifernandeaz/francastyle-grupo-17.git', '_blank');
+}
+
+function openDemoTrancaStyle() {
+    window.open('https://fracastyle.netlify.app/', '_blank');
+}
+
+
+
